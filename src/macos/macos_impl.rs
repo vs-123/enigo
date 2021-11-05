586a6d1acf4c7e6b161aa40e67ca1ee030ee1447
vs-123 <19247154+vs-123@users.noreply.github.com>
2021-11-05 15:56:41 +0530

    Update macos_impl.rs

--- src/macos/macos_impl.rs
+++ src/macos/macos_impl.rs
@@ -432,7 +432,7 @@ impl Enigo {
     }
 
     /// The mouse coordinates in points, only works on the main display
-    pub fn mouse_location() -> (i32, i32) {
+    pub fn mouse_location(&mut self) -> (i32, i32) {
         let (x, y_inv) = Self::mouse_location_raw_coords();
         let (_, display_height) = Self::main_display_size();
         (x, (display_height as i32) - y_inv)
